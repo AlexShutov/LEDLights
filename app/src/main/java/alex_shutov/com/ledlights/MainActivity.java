2d571b61e448e8934df2bf0ee16f38aea76a39ec
User <lodoss@lodoss.com>
2016-06-30 13:56:20 +0300

    project created

---
+++ app/src/main/java/alex_shutov/com/ledlights/MainActivity.java
@@ -0,0 +1,20 @@
+package alex_shutov.com.ledlights;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btnTest);
+        btn.setOnClickListener(v -> {
+
+        });
+
+    }
+}
