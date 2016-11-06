806c384e626ae72b50be5f25d184d93279a1fedd
AlexShutov <Alekcey_Shutov@mail.ru>
2016-11-06 01:08:09 -0700

    Test Activity listens for all events coming from CommPort.

--- app/src/main/java/alex_shutov/com/ledlights/BtCellActivity.java
+++ app/src/main/java/alex_shutov/com/ledlights/BtCellActivity.java
@@ -2,10 +2,13 @@ package alex_shutov.com.ledlights;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import alex_shutov.com.ledlights.bluetoothmodule.bluetooth.BtCommPort.hex.BtCommPort;
+import alex_shutov.com.ledlights.bluetoothmodule.bluetooth.BtCommPort.hex.BtCommPortListener;
+import alex_shutov.com.ledlights.bluetoothmodule.bluetooth.BtDevice;
 import alex_shutov.com.ledlights.bluetoothmodule.bluetooth.BtLogicCell;
 import alex_shutov.com.ledlights.hex_general.LogicCell;
 
@@ -13,6 +16,7 @@ import alex_shutov.com.ledlights.hex_general.LogicCell;
  * Created by Alex on 10/20/2016.
  */
 public class BtCellActivity extends Activity {
+    private static final String LOG_TAG = BtCellActivity.class.getSimpleName();
     Button btnStart;
     TextView tvPrint;
     LEDApplication app;
@@ -31,6 +35,54 @@ public class BtCellActivity extends Activity {
         btCell = app.getCell();
         btnStart.setOnClickListener(v -> {
             startPolling();
+        });
+
+        btCell.setBtCommPortListener(new BtCommPortListener() {
+            @Override
+            public void onConnectionStarted(BtDevice btDevice) {
+                Log.i(LOG_TAG, "onConnectionStarted(): " + btDevice.getDeviceName());
+            }
+
+            @Override
+            public void onConnectionFailed() {
+                Log.i(LOG_TAG, "onConnectionFailed()");
+            }
+
+            @Override
+            public void onDataSent() {
+                Log.i(LOG_TAG, "onDataSent()");
+            }
+
+            @Override
+            public void onDataSendFailed() {
+                Log.i(LOG_TAG, "onDataSendFailed()");
+            }
+
+            @Override
+            public void onReconnected(boolean isSameDevice) {
+                Log.i(LOG_TAG, "onReconnected(), is same device? " + isSameDevice);
+            }
+
+            @Override
+            public void onDummyDeviceSelected() {
+                Log.i(LOG_TAG, "onDummyDeviceSelected()");
+            }
+
+            @Override
+            public void onReconnectAttemptFailed() {
+                Log.i(LOG_TAG, "onReconnectAttemptFailed()");
+            }
+
+            @Override
+            public void onPortReady(int portID) {
+                Log.i(LOG_TAG, "onPortReady(), id: " + portID);
+            }
+
+            @Override
+            public void onCriticalFailure(int portID, Exception e) {
+                Log.i(LOG_TAG, "onCriticalFailure(), id: " + portID + " message: " +
+                    e.getMessage());
+            }
         });
     }
 
